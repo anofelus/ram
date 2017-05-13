8815b3eda328c7453830ed8fd9e650fda764ec13
anofelus <anofelus@icloud.com>
2017-05-13 17:16:35 +0900

    モーダルの設定、プロフ画像など追加

--- js/func.js
+++ js/func.js
@@ -1,5 +1,11 @@
 $(function() {
-  $('.about-me-show').click(function(){
-    $('#about-me').fadeIn();
-  })
+
+  $('.about-me-show').click(function() {
+  $('#about-me').fadeIn();
+  });
+
+  $('.back').click(function() {
+  $('#about-me').fadeOut();
+  });
+
 });
